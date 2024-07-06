f9fe49a26ea152a711e56d4b624610305f284f13
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-07-06 10:41:23 +0200

    jour 5 - alog de tri bubble et insert 8/10 => gymnastique cérébrale

---
+++ Juillet/05.Jour05-Tri/app.ts
@@ -0,0 +1,93 @@
+"use strict";
+
+/*
+### Jour 5 : Algorithmes de tri
+
+**Rappel :**
+
+- Introduction aux algorithmes de tri (tri à bulles, tri par insertion, tri par sélection)
+- Comparaison et choix des algorithmes
+
+**Exercice :**
+
+1. Implémenter un tri à bulles pour un tableau de nombres.
+*/
+function bubbleSort(argArray: number[]): number[] | undefined | null {
+  const length = argArray.length;
+  let swapped: boolean;
+
+  try {
+    if (argArray.length === 0 || argArray === undefined || argArray === null) {
+      throw new Error(`Veuillez saisir un tableau de nombres non vide!`);
+    }
+    if (!Array.isArray(argArray)) {
+      throw new Error(`Veuillez saisir un tableau de nombres!`);
+    }
+    if (argArray.some((element) => isNaN(element))) {
+      throw new Error(
+        `Veuillez saisir un tableau contenant uniquement des nombres!`
+      );
+    }
+
+    // logique :
+    do {
+      swapped = false;
+      for (let i = 0; i < length - 1; i++) {
+        if (argArray[i] > argArray[i + 1]) {
+          [argArray[i], argArray[i + 1]] = [argArray[i + 1], argArray[i]];
+          swapped = true;
+        }
+      }
+    } while (swapped);
+
+    return argArray;
+  } catch (error) {
+    console.error("Erreur : ", error);
+  }
+}
+
+// Test :
+const numbersArray: number[] = [64, 34, 25, 12, 22, 11, 90];
+const result: string = `Tableau avant le triage à bulles : ${numbersArray}
+Tableau après le triage à bulle : ${bubbleSort(numbersArray)}
+`;
+console.log(result);
+
+/* 2. Implémenter un tri par insertion pour un tableau de nombres.*/
+
+function insertSort(argArray: number[]) {
+  try {
+    if (argArray.length === 0 || argArray === undefined || argArray === null) {
+      throw new Error(`Veuillez saisir un tableau de nombres non vide!`);
+    }
+    if (!Array.isArray(argArray)) {
+      throw new Error(`Veuillez saisir un tableau de nombres!`);
+    }
+    if (argArray.some((element) => isNaN(element))) {
+      throw new Error(
+        `Veuillez saisir un tableau contenant uniquement des nombres!`
+      );
+    }
+
+    // logique :
+    for (let i = 0; i < argArray.length; i++) {
+      let currentValue = argArray[i];
+      let j = i - 1;
+      while (j >= 0 && argArray[j] > currentValue) {
+        argArray[j + 1] = argArray[j];
+        j--;
+      }
+
+      argArray[j + 1] = currentValue;
+    }
+    return argArray;
+  } catch (error) {
+    console.error("Erreur : ", error);
+  }
+}
+
+// test :
+const secondResult: string = `Tableau avant le triage par insertion : ${numbersArray}
+Tableau après le triage par insertion : ${insertSort(numbersArray)}
+`;
+console.log(secondResult);
--- Juillet/src/app.js
+++ Juillet/src/app.js
@@ -1,53 +1,14 @@
 /*
-### Jour 4 : Manipulation des tableaux et des chaînes de caractères
+### Jour 5 : Algorithmes de tri
 
 **Rappel :**
 
-- Méthodes de tableau courantes (push, pop, shift, unshift, map, filter, reduce)
-- Méthodes de chaîne de caractères (length, indexOf, slice, substring)
+- Introduction aux algorithmes de tri (tri à bulles, tri par insertion, tri par sélection)
+- Comparaison et choix des algorithmes
 
 **Exercice :**
 
-1. Écrire un programme qui inverse une chaîne de caractères.
+1. Implémenter un tri à bulles pour un tableau de nombres.
+2. Implémenter un tri par insertion pour un tableau de nombres.
 
 */
-function reverseString(argString) {
-    try {
-        if (argString === null || argString === undefined || argString === "") {
-            throw new Error("Veuillez saisir une chaîne de caractères non vide !");
-        }
-        if (typeof argString != "string") {
-            throw new Error("Veuillez saisir une chaîne de caractères");
-        }
-        var reversedString = argString.split("").reverse().join("");
-        return reversedString;
-    }
-    catch (error) {
-        return error.message;
-    }
-}
-console.log("Function reverseString");
-console.log(reverseString("Hello world"));
-/* test */
-var testStringArray = [
-    null,
-    undefined,
-    "",
-    "Hello world",
-    9,
-    ["bonjour"],
-    [9],
-    { a: "b" },
-];
-var testReverseStringFunction = testStringArray.forEach(function (element) {
-    return console.log(reverseString(element));
-});
-console.log("Test de la function reverseString :", testReverseStringFunction);
-/* 2. Écrire un programme qui filtre les nombres pairs d'un tableau donné. */
-function filterPairNumberForArray(argArray) {
-    var filteredPairNumber = argArray.filter(function (number) { return number % 2 === 0; });
-    return filteredPairNumber;
-}
-var testNumberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log("Function filterPairNumberForArray");
-console.log(filterPairNumberForArray(testNumberArray));

87b5d42c3160885533089e421466a2e38f233a81
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-07-08 17:16:58 +0200

    algo juillet s2 - j1 => 8/10 => qq hs

--- Juillet/src/app.ts
+++ Juillet/src/app.ts
@@ -39,46 +39,32 @@ console.log(reverseString(testString));
 Écrire une fonction qui prend un tableau de nombres et retourne un nouvel objet avec deux propriétés : "pair" pour les nombres pairs et "impair" pour les nombres impairs.
  */
 
-function isPairOrUnPair(paramNumberArray: number[]) {
-  try {
-    if (
-      paramNumberArray === undefined ||
-      paramNumberArray === null ||
-      paramNumberArray.length === 0
-    ) {
-      throw new Error(`Veuillez saisir un tableau d'éléments non vide !`);
-    }
-    if (!Array.isArray(paramNumberArray)) {
-      throw new Error(`Veuillez saisir un tableau`);
-    }
-    if (paramNumberArray.some((element) => !Number.isInteger(element))) {
-      throw new Error(
-        `Veuillez saisir un tableau contenant uniquement des nombres entiers`
-      );
-    }
-
-    if (paramNumberArray.some((element) => element < 0)) {
-      throw new Error(
-        `Veuillez saisir un tableau contenant uniquement des nombres entiers positif et supérieur à 0`
-      );
-    }
-
-    // main logic :
-    let resultArray: object[] = [];
-    for (let i = 0; i < paramNumberArray.length; i++) {
-      if (paramNumberArray[i] % 2 === 0) {
-        resultArray.push({ pair: paramNumberArray[i] });
-      } else {
-        resultArray.push({ impair: paramNumberArray[i] });
-      }
-    }
-    return resultArray;
-  } catch (error: unknown) {
-    console.error("Une erreur s'est produite : ", error);
-    return null;
+function separateEvenOddNumbers(paramNumberArray: number[]): {
+  pair: number[];
+  impair: number[];
+} {
+  if (!Array.isArray(paramNumberArray) || paramNumberArray.length === 0) {
+    throw new Error("Veuillez saisir un tableau d'éléments non vide !");
   }
+  if (paramNumberArray.some((element) => !Number.isInteger(element))) {
+    throw new Error(
+      "Veuillez saisir un tableau contenant uniquement des nombres entiers"
+    );
+  }
+
+  const result = { pair: [], impair: [] };
+
+  for (const num of paramNumberArray) {
+    if (num % 2 === 0) {
+      result.pair.push(num);
+    } else {
+      result.impair.push(num);
+    }
+  }
+
+  return result;
 }
 
 const testNumberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log("Exercice 2 - isPairOrUnpair");
-console.log(isPairOrUnPair(testNumberArray));
+console.log(separateEvenOddNumbers(testNumberArray));
+// { pair: [0, 2, 4, 6, 8, 10, 12], impair: [1, 3, 5, 7, 9, 11] }
